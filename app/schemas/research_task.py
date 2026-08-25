b5084bd9d7369a7ebfa4e6a3bd812bccdc258c35
Phạm Ngọc Quỳnh Như <mina03239@gmail.com>
2026-08-25 20:57:41 +0700

    commit

--- app/schemas/research_task.py
+++ app/schemas/research_task.py
@@ -20,11 +20,11 @@ class ResearchTaskCreate(BaseModel):
 
 
 class ResearchTaskUpdate(BaseModel):
-    title: str 
+    title: str
     description: str 
-    assignee_id: int
-    status: str
-    priority: str
+    assignee_id: int 
+    status: str 
+    priority: str 
     due_date: datetime 
 
 
